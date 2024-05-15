9d77b25a0854b96613b9038e0706a0067f4a9bc5
Ron Javellana <Ronapj1991@gmail.com>
2024-05-15 17:10:53 +0000

    add test for show page

--- spec/requests/authors_spec.rb
+++ spec/requests/authors_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe "Authors", type: :request do
       expect(response.body.include?("Authors")).to be true
     end
   end
+  
+  describe "GET /authors/:id" do
+    it "displays the show page" do
+      author = Author.first
+      get author_path(author)
+      expect(response).to have_http_status(200)
+    end
+  end
 end
