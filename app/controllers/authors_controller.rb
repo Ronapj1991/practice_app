69931cc88c38051dc8f4802636ca3d98d3c0afe6
Ron Javellana <Ronapj1991@gmail.com>
2024-05-21 18:03:48 +0000

    add edit author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -21,6 +21,20 @@ class AuthorsController < ApplicationController
     end
   end
   
+  def edit
+    @author = Author.find(params[:id])
+  end
+  
+  def update
+    @author = Author.find(params[:id])
+    
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
   private
     def author_params
       params.require(:author).permit(:name)
